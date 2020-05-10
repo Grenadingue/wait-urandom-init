a8896c6a853cb4b0b7d97ec9665e521bd2f97fce
Nicolas Chauvin <chauvin.nico@gmail.com>
2020-05-10 20:09:06 +0200

    WaitUrandomInit: Implement a simple version of the tool
    by using getrandom() system call

--- main.c
+++ main.c
@@ -1,4 +1,19 @@
+#include <sys/random.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+
 int main(void)
 {
+  char buffer[256];
+  int ret = -1;
+
+  if ((ret = getrandom(buffer, sizeof(buffer), 0)) != sizeof(buffer))
+  {
+    fprintf(stderr, "Error: getrandom() failure, returned: %d, error message: '%s'\n", ret, strerror(errno));
+    return 1;
+  }
+
+  printf("Success: urandom source is initialized\n");
   return 0;
 }
